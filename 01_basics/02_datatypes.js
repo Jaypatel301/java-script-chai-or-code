8a90798cae2a807dc7c4f8a444b78cdfb19414b0
Jaypatel301 <153203819+Jaypatel301@users.noreply.github.com>
2026-03-05 10:45:34 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // ye sare code ko newer version js code ki tarah treat karta hai
+
+let name="jay"
+let age=22
+
+//int => 2 to power 23
+// bigint
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol =>unique
+
+// object
