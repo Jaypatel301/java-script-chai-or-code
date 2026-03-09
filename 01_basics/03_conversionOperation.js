cc5888b6851b33723db2e7311a73277353f9a2ec
Jaypatel301 <153203819+Jaypatel301@users.noreply.github.com>
2026-03-09 10:36:43 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,7 +28,45 @@
 // 0=>false, ""=>false, "jay"=>true
 
 //--------------------------------------------------------------------------------
-let someNumber=33;
-let stringNumber=String(someNumber); // "33"
-console.log(typeof(stringNumber)); // string
+//let someNumber=33;
+//let stringNumber=String(someNumber); // "33"
+//console.log(typeof(stringNumber)); // string
+
+//*************************************operations*********************************
+//let value=3;
+//let negvalue=-val; // -3
+//console.log(2+3);
+//console.log(2-3);
+//console.log(2*3);
+//console.log(2/3);
+//console.log(2**3);
+//console.log(2%3);
+
+//let str1="hello";
+//let str2=" jay";
+//let str3=str1+str2; // "hello jay"
+
+//console.log("1"+2); // "12"
+//console.log(1+"2"); // "12"
+//console.log("1"+2+2); // "122"
+//console.log(1+2+"2"); // "32"
+//console.log(true); // true
+//console.log(+true); // 1
+//console.log(+""); // 0
+
+//let a,b,c;
+//a=b=c=2+2;
+//console.log(a,b,c); // a=4,b=4,c=4
+
+//let value=100;
+//value++; // value=101
+//++ value; // value=102
+
+//let x=10;
+//y=x++; // y=10,x=11
+//console.log(x,y);
+
+let value=10;
+let newval=++value; // value=11, newval=11
+console.log(value,newval);
 
