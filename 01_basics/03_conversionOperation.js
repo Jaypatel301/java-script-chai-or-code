c1eae67948291f1c2f86040cb227a63869a97304
Jaypatel301 <153203819+Jaypatel301@users.noreply.github.com>
2026-03-09 07:44:32 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+//let value="33";
+//console.log(typeof(value)); //string
+//let valueInNumber=Number(value); //valueInNumber=33
+//console.log(typeof(valueInNumber)); // Number
+
+//----------------------------------------------------------------------------------
+//let value="33abc";
+//console.log(typeof(value)); //string
+//let valueInNumber=Number(value); //NaN
+//console.log(valueInNumber); // NaN
+//console.log(typeof(valueInNumber)); // Number
+
+//----------------------------------------------------------------------------------
+//let value=null;
+//let valueInNumber=Number(value); // 0
+
+//---------------------------------------------------------------------------------
+//let value=undefined;
+//let valueInNumber=Number(value); //NaN
+
+//---------------------------------------------------------------------------------
+//let value=true;
+//let valueInNumber=Number(value) // 1
+
+//-------------------------------------------------------------------------------
+//let isLoggedin=1;
+//let boolisLoggedin=Boolean(isLoggedin); // true
+// 0=>false, ""=>false, "jay"=>true
+
+//--------------------------------------------------------------------------------
+let someNumber=33;
+let stringNumber=String(someNumber); // "33"
+console.log(typeof(stringNumber)); // string
+
