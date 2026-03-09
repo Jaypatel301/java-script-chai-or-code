53b9d4d9b2c8c77cf3f1fd640dfa66f387aad5ee
Jaypatel301 <153203819+Jaypatel301@users.noreply.github.com>
2026-03-09 11:16:52 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+//console.log(2>1);
+//console.log(2<1);
+//console.log(2>=1);
+//console.log(2<=1);
+//console.log(1==2);
+//console.log(1!=2);
+
+//console.log("2">1); //true
+//console.log("02">1); // true
+
+//console.log(null>0); // false
+//console.log(null>=0); // true
+//console.log(null==0); // false
+/*
+   isme == and >,>=,<,<= alag tarike se kam karte hai
+   isme null ko 0 mankar >,<=,<,>= se compare karte hai
+*/
+
+//console.log(undefined>0); // false
+//console.log(undefined>=0); // false
+//console.log(undefined==0); // false
+
+console.log("2"==2); // true : == conversion karke check karta hai matlab data type check nahi karta
+console.log("2"===2);// false: === conversion nahi karta direct value check karta hai. matlab ye data type bhi check karta hai
+
