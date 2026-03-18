24f2cd857a83e0e3e490ff3dc1e70f45ffd65192
Jaypatel301 <153203819+Jaypatel301@users.noreply.github.com>
2026-03-18 17:13:12 +0000

    while , do while loop in js

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+let i=0;
+while(i<=10)
+{
+    //console.log(`the value of i is ${i}`);
+    i=i+2;
+}
+
+let myArray=["flash","batman","superman"];
+let index=0;
+while(index<myArray.length)
+{
+    //console.log(`the value is ${myArray[index]}`);
+    index=index+1;
+}
+
+let score=1;
+
+do{            // pahli bar to ye code chal hi jayega
+    console.log(`score is ${score}`);
+    score++;
+}
+while(score<=10);
